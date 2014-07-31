4709a63c348f749d29f55777dbe16c24f5c56ddb
zhixin <wenzhixin2010@gmail.com>
2014-07-31 15:09:22 +0800

    add hooks.

--- config.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author zhixin <wenzhixin2010@gmail.com>
- */
-
-module.exports = {
-	site: 'http://wenzhixin.net.cn'
-};
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,38 @@
  * @date 2013-03-28
  */
 
-var post = require('./post'),
-	rss = require('./rss'),
-	stat = require('./stat'),
-    examples = require('./examples');
+var shelljs = require('shelljs'),
+    post = require('./post'),
+    rss = require('./rss'),
+    stat = require('./stat'),
+    examples = require('./examples'),
+    config = require('../config');
 
-module.exports = function(app) {
-	app.get('/', checkUrl);
-	app.get('/:file', checkUrl);
-	app.get('/:yy/:mm/:dd/:file', checkUrl);
+module.exports = function (app) {
+    app.get('/', checkUrl);
+    app.get('/:file', checkUrl);
+    app.get('/:yy/:mm/:dd/:file', checkUrl);
     app.get('/examples/:project/:func', examples);
+    app.post('/hooks/' + config.key, autoBuild);
 };
 
 function checkUrl(req, res) {
-	if (req.params.file === 'rss.xml') {
-		rss.list(req, res);
-		return;
-	}
-	if (req.params.file === 'stat') {
-		stat.visits(req, res);
-		return;
-	}
-	post.get(req, res);
+    if (req.params.file === 'rss.xml') {
+        rss.list(req, res);
+        return;
+    }
+    if (req.params.file === 'stat') {
+        stat.visits(req, res);
+        return;
+    }
+    post.get(req, res);
+}
+
+function autoBuild(req, res) {
+    var command = 'git pull --rebase; node build';
+    shelljs.exec(command, function (code, output) {
+        console.log(output);
+        res.status(200);
+        res.end();
+    });
 }
